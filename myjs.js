e500f5aff9d0e67f54206f69aabbd159dfbe4c9b
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-02-14 15:28:58 +0800

    mystylesheet

---
+++ myjs.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+
+
+function openNav2() {
+  document.getElementById("myNav2").style.width = "100%";
+}
+
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+}
