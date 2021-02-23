74be3c1eb04f9e1868e8d698e0808b9ead4d2cce
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-02-23 17:00:05 +0800

    new changes

--- myjs.js
+++ myjs.js
@@ -1,3 +1,4 @@
+// from top to bottom iq and eq test
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
 }
@@ -6,12 +7,40 @@ function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
 
+// from bottom to top personality test
+function openNav4() {
+  document.getElementById("myNav4").style.height = "100%";
+}
 
+function closeNav4() {
+  document.getElementById("myNav4").style.height = "0%";
+}
 
+// from left to right career path
 function openNav2() {
   document.getElementById("myNav2").style.width = "100%";
 }
 
 function closeNav2() {
   document.getElementById("myNav2").style.width = "0%";
+}
+
+// from right to left competencies and skills
+	function openNav3() {
+  document.getElementById("myNav3").style.width = "100%"; /** from 100% to 25% **/
+}
+
+function closeNav3() {
+  document.getElementById("myNav3").style.width = "0";
+}
+
+
+
+// from bottom to top multiple intelligence
+function openNav4multiple() {
+  document.getElementById("myNav5").style.height = "100%";
+}
+
+function closeNav5() {
+  document.getElementById("myNav5").style.height = "0%";
 }
