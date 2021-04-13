e0dee481c0d19571dcc4045a8193033cd83de366
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-04-13 15:58:58 +0800

    new era

---
+++ gamepage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<a href="mygames/typinggameweb.php">Typing game</a>
+	<br>
+	<a href="mygames/puzzlegame.php">Puzzle game</a>
+
+</body>
+</html>
