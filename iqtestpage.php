534b079c2d7701bb450312565c51c6f3379e5258
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-05-04 14:08:26 +0800

    Update iqtestpage.php

--- iqtestpage.php
+++ iqtestpage.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>IQ TEST</title>
+<title>GnothiSeauton - IQ TEST</title>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
