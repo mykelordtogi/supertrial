c229deae885612e0fd25df5f7626a99ba201ba3b
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-05-04 13:55:40 +0800

    Update gamepage.php

--- gamepage.php
+++ gamepage.php
@@ -1,13 +1,87 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	 <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>GnothiSeauton - Game Page</title>
+
+	 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+<meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="toknow.png" rel="icon">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
 </head>
+
+<style type="text/css">
+	
+    body{
+          font-family: sans-serif;
+  background-image: url("assets/img/hero-bg.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: scroll;
+  
+    }
+
+    #thumb1
+    {
+    	position: absolute;
+    	height: 200px;
+    	width: 250px;
+    	left: 25%;
+    	top: 25%;
+    }
+
+       #thumb2
+    {
+    	position: absolute;
+    	height: 210px;
+    	width: 250px;
+    		left: 50%;
+    	top: 24%;
+    }
+    #link1
+    {
+    	position: absolute;
+    	top: 380px;
+    	left: 380px;
+    }
+
+        #link2
+    {
+    	position: absolute;
+    	top: 380px;
+    	left: 700px;
+    }
+
+</style>
+
 <body>
-
-	<a href="mygames/typinggameweb.php">Typing game</a>
+	<img id="thumb1" src="thumbnail.png">
+	<a id="link1" href="mygames/typinggameweb.php">Typing game</a>
+	
 	<br>
-	<a href="mygames/puzzlegame.php">Puzzle game</a>
-
+	<img id="thumb2" src="thumbnail2.png">
+	<a id="link2" href="mygames/puzzlegame.php">Puzzle game</a>
+	
 </body>
 </html>
